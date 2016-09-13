9a87d9b685cb60e03a38cb3e325c4f6476c9f837
MrRoboman <rkayson@gmail.com>
2016-09-12 23:56:19 -0700

    welcome screen styling

--- index.js
+++ index.js
@@ -14,4 +14,40 @@ app.use(express.static(__dirname + '/public'))
 app.use(webpackDevMiddleware(webpack(webpackConfig)))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+const clients = {}
+function broadcastPlayers() {
+  const players = []
+  for(var key in clients) {
+    if(clients[key].name)
+      players.push(clients[key])
+  }
+  io.sockets.emit('players', players)
+}
+
+io.on('connection', socket => {
+
+  clients[socket.id] = { name: '' }
+
+  socket.on('join', name => {
+    clients[socket.id].name = name
+    socket.broadcast.emit('join', name)
+    broadcastPlayers()
+  })
+
+  socket.on('disconnect', () => {
+    const name = clients[socket.id].name
+    delete clients[socket.id]
+    socket.broadcast.emit('leave', name)
+    broadcastPlayers()
+  })
+
+  socket.on('message', body => {
+    socket.broadcast.emit('message', {
+      body,
+      from: socket.id.slice(8)
+    })
+  })
+
+})
+
 server.listen(3000)
--- src/index.js
+++ src/index.js
@@ -3,10 +3,61 @@ import ReactDOM from 'react-dom'
 import io from 'socket.io-client'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { name: '' }
+  }
+
+  componentDidMount() {
+    document.addEventListener('touchmove', function(e) {
+    	e.preventDefault();
+    }, false);
+
+    this.socket = io('/')
+
+    this.socket.on('join', name => {
+      console.log(name + " joined!")
+    })
+
+    this.socket.on('leave', name => {
+      console.log(name + " left!")
+    })
+
+    this.socket.on('players', players => {
+      console.log(players)
+      this.setState({ players: players })
+    })
+  }
+
+  onChange = event => {
+    this.setState({ name: event.target.value })
+  }
+
+  handleSubmit = event => {
+    this.socket.emit('join', name)
+    this.setState({ nextPage: true })
+  }
+
+  renderButton = () => {
+    if(/^\S/.test(this.state.name)) {
+      return <button onClick={this.handleSubmit}>enter the game</button>
+    }
+  }
+
   render() {
+    if(this.state.nextPage) {
+      return <span>Game on {this.state.name}</span>
+    }
     return (
-      <div>
-        <h1>Hello World!</h1>
+      <div className="welcome">
+        <h1>Wise Cracks!</h1>
+        <input
+          type='text'
+          placeholder='enter your name'
+          value={this.state.name}
+          onChange={this.onChange}
+          />
+        {this.renderButton()}
       </div>
     )
   }
